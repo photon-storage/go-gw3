64ec9bccea268cb6e0063a395c568b0360cb0dfc
kmax.eth <92766553+maxkaneco@users.noreply.github.com>
2023-05-21 16:19:06 +0800

    export hostname config (#3)

--- common/net/hostname.go
+++ common/net/hostname.go
@@ -6,8 +6,13 @@ import (
 	"fmt"
 )
 
+const (
+	HashLen    = 16
+	DomainName = "gw3.io"
+)
+
 func GW3Hostname(ip string) string {
 	sum := sha256.Sum256([]byte(ip))
 	h := hex.EncodeToString(sum[:])
-	return fmt.Sprintf("%s.gw3.io", h[:16])
+	return fmt.Sprintf("%s.%s", h[:HashLen], DomainName)
 }
