525797b560dd54cddb22bdb0bc18b66cd030e565
kmax.eth <max.kaneco@gmail.com>
2023-07-06 19:44:39 +0800

    Add debug fields to jwt

--- common/auth/jwt_test.go
+++ common/auth/jwt_test.go
@@ -11,6 +11,8 @@ func TestJWT(t *testing.T) {
 		AccountID: 996,
 		Provider:  "mock_provider",
 		Username:  "mock_name",
+		Perm:      31,
+		Debug:     "k0=v0; k1= v1",
 	}
 
 	secert := []byte("ICU")
@@ -19,4 +21,9 @@ func TestJWT(t *testing.T) {
 	got, err := NewClaimsFromJWT(jwt, secert)
 	require.NoError(t, err)
 	require.DeepEqual(t, origin, got)
+
+	kvs := got.ExtractDebug()
+	require.Equal(t, 2, len(kvs))
+	require.Equal(t, "v0", kvs["k0"])
+	require.Equal(t, "v1", kvs["k1"])
 }
