0be39dd5481b363269757e079e1dd0a9470072fd
kmax.eth <92766553+maxkaneco@users.noreply.github.com>
2023-09-01 11:04:39 +0800

    update access code len (#25)
    * update access code len
    
    * fix test

--- common/auth/accesscode.go
+++ common/auth/accesscode.go
@@ -8,18 +8,26 @@ import (
 )
 
 const (
-	acPrefix = "1f"
-	acLen    = 10
+	acPrefix = "1"
+	acLen    = 7
 )
 
 var (
 	ErrAccessCodeNotFound = errors.New("access code not found")
 )
 
+// Common CID in b36 encoding is usually 56 bytes.
+// The max length for a domain label is 63 bytes.
+// With 1 byte prefix identifier, it leaves 6 bytes for access code.
+// A hex encoding allows actual 3 bytes of data: 2^24 = 16M variations.
 func GenAccessCode() string {
 	token := make([]byte, (acLen-len(acPrefix))/2)
 	rand.Read(token)
-	return "1f" + hex.EncodeToString(token)
+	return acPrefix + hex.EncodeToString(token)
+}
+
+func AccessCodeLen() int {
+	return acLen
 }
 
 func HasAccessCodePrefix(str string) bool {
